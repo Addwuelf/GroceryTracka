8bf7c376c93fc2f5c64fc688ea243738ab160307
Addwuelf <Addwuelf@yahoo.com>
2025-05-09 00:08:23 -0400

    Created GroceryList attribute as well as started working on GroceryHomeLists views.

---
+++ GroceryTracka/GroceryListsViews/HomeListsView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeListsView.swift
+//  GroceryTracka
+//
+//  Created by Adam Wuelfing on 5/8/25.
+//
+
+import SwiftUI
+
+struct HomeListsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeListsView()
+}
