14537edce47d54dfd2b62429eca3dfd0d2f1dfa9
Addwuelf <Addwuelf@yahoo.com>
2025-05-10 01:00:20 -0400

    Completed most of  HomeListView. User is able to view all created grocery lists, delete grocery lists, create a new grocery list, change the name of a grocery list, and view the items on their grocery list. Need to add input validation to make sure they cant create a new grocery list with a enpty name. As well as edit the GroceryListView to use the GroceryList entity for displaying items.

--- GroceryTracka/GroceryListsViews/HomeListsView.swift
+++ GroceryTracka/GroceryListsViews/HomeListsView.swift
@@ -6,13 +6,148 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct HomeListsView: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @FetchRequest(
+        entity: GroceryList.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \GroceryList.listname, ascending: true)],
+        animation: .default)
+    private var lists: FetchedResults<GroceryList>
+    
+    
+    @State var selectedGroceryList: GroceryList?
+    @State private var listName = ""
+    @State private var newListName = ""
+    @State private var showingAlert = false
+    @State private var showingEditAlert = false
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Text("GroceryTracka").font(.largeTitle)
+            NavigationStack() {
+                ZStack{
+                    VStack {
+                        List {
+                            ForEach(lists) { list in
+                                
+                                HStack {
+                                    // Contentviews View Triggered by Clicking the Text
+                                    NavigationLink(destination: Contetview()) {
+                                        Text(list.listname ?? "default value")
+                                            .contentShape(Rectangle())
+                                    }
+                                    
+                                    // Recipe View Triggered by Clicking the Info Icon
+                                    Button(action: {
+                                        showingEditAlert = true
+                                        newListName = list.listname ?? "New List"
+                                        selectedGroceryList = list
+                                        
+                                    }) {
+                                        Image(systemName: "info.circle")
+                                            .foregroundColor(.blue)
+                                            .padding()
+                                    }
+                                    .buttonStyle(BorderedButtonStyle())
+                                    .alert("List Name", isPresented: $showingEditAlert) {
+                                        
+                                        VStack {
+                                            TextField("", text: $newListName)
+                                        }
+                                        Button("Ok", role: .cancel) {
+                                            listName = newListName
+                                            showingAlert = false
+                                            addAction()
+                                        }
+                                        
+                                    }
+                                }
+                            }
+                            .onDelete(perform: deleteItem)
+                            
+                        }
+                    }
+                    
+                    .toolbar() {
+                        // Allows user to delete items
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            EditButton()
+                        }
+                        // When clicked displays a alert
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button(" + "){
+                                showingAlert = true
+                            }
+                            .font(.headline)
+                           // Alert allows user to create a new Grocery List
+                            .alert("Enter List Name", isPresented: $showingAlert) {
+                                
+                                VStack {
+                                    TextField("", text: $newListName)
+                                }
+                                Button("Ok", role: .cancel) {
+                                    listName = newListName
+                                    showingAlert = false
+                                    selectedGroceryList = nil
+                                    addAction()
+                                }
+                                
+                            }
+                            
+                        }
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+    // Allow deletion of a Grocery List
+    func deleteItem(at offsetss: IndexSet) {
+        offsetss.map {lists[$0]}.forEach(viewContext.delete)
+        
+        saveContext(viewContext)
+        
+    }
+    
+    func addAction() {
+        withAnimation {
+            if let list = selectedGroceryList {
+                    // If there's an existing list, updates its name instead of creating a new one
+                    list.listname = newListName
+                } else {
+                    // If no list is selected, creates a new one
+                    selectedGroceryList = GroceryList(context: viewContext)
+                    selectedGroceryList?.listname = newListName
+                }
+            newListName = ""
+            do {
+                // Trys to save the changes
+                try viewContext.save()
+            } catch {
+                // Display errors
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError) \(nsError.userInfo)")
+            }
+        }
+    }
+    
+    func saveContext(_ context: NSManagedObjectContext ) {
+        do {
+            try context.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError) \(nsError.userInfo)")
+        }
     }
 }
-
 #Preview {
     HomeListsView()
 }
